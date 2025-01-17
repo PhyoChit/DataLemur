db0bb562fd88944521ed13e0400ff7a961832062
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2025-01-17 13:10:44 +0100

    Update Card Launch Success (JP Morgan).sql

--- SQL/Card Launch Success (JP Morgan).sql
+++ SQL/Card Launch Success (JP Morgan).sql
@@ -3,4 +3,20 @@ SELECT
   FIRST_VALUE(issued_amount) OVER (PARTITION BY card_name
   ORDER BY issue_year, issue_month) AS issued_amount
 FROM monthly_cards_issued
+ORDER BY issued_amount DESC;
+
+-- Option 2
+
+WITH card_launch AS (
+  SELECT 
+    card_name,
+    issued_amount,
+    MAKE_DATE(issue_year, issue_month, 1) AS issued_date,
+    MIN(MAKE_DATE(issue_year, issue_month, 1)) OVER (PARTITION BY card_name) AS launch_date
+  FROM monthly_cards_issued
+)
+
+SELECT card_name, issued_amount
+FROM card_launch
+WHERE issued_date = launch_date
 ORDER BY issued_amount DESC
