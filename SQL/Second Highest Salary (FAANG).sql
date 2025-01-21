f13e338b83a98da0e9a03c221773afba537a4921
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2025-01-21 14:14:33 +0100

    Update Second Highest Salary (FAANG).sql

--- SQL/Second Highest Salary (FAANG).sql
+++ SQL/Second Highest Salary (FAANG).sql
@@ -9,4 +9,10 @@ WITH salary_ranking AS (
 
 SELECT DISTINCT salary AS second_highest_salary
 FROM salary_ranking
-WHERE salary_rank = 2
+WHERE salary_rank = 2;
+
+-- Option 2
+
+SELECT MAX(salary) AS second_highest_salary
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee)
