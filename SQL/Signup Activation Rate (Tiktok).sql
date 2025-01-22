f6476eb860cb554086dc23d41e4d77d7939a2464
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2025-01-22 20:18:36 +0100

    Update Signup Activation Rate (Tiktok).sql

--- SQL/Signup Activation Rate (Tiktok).sql
+++ SQL/Signup Activation Rate (Tiktok).sql
@@ -1,6 +1,7 @@
 SELECT 
-    ROUND(SUM(CASE WHEN t.signup_action = 'Confirmed' THEN 1 ELSE 0 END) * 1.0/
-    SUM(CASE WHEN t.signup_action IN ('Confirmed', 'Not Confirmed') THEN 1 ELSE 0 END), 2) AS confirm_rate
-FROM emails AS e 
-JOIN texts AS t 
+    ROUND( COUNT(t.email_id)::DECIMAL /
+    COUNT(DISTINCT e.email_id), 2) AS confirm_rate
+FROM emails AS e
+LEFT JOIN texts AS t
 ON e.email_id = t.email_id
+AND t.signup_action = 'Confirmed'
