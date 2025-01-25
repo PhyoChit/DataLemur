4cad3259f948ddadc0b3e9bc1f3732915f243c41
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2025-01-26 00:01:28 +0100

    Update Maximize Prime Item Inventory (Amazon).sql

--- SQL/Maximize Prime Item Inventory (Amazon).sql
+++ SQL/Maximize Prime Item Inventory (Amazon).sql
@@ -1,21 +1,20 @@
 WITH summary AS (
-  SELECT 
-    item_type,
-    SUM(square_footage) AS sqf,
-    COUNT(item_id) AS item_count
+  SELECT item_type,
+         SUM(square_footage) AS sqf,
+         COUNT(item_id) AS item_count
   FROM inventory
   GROUP BY item_type
 )
 
-SELECT item_type, 
-  FLOOR(500000/sqf)* item_count AS item_count
-FROM cte
+SELECT item_type,
+       FLOOR(500000/sqf) * item_count AS item_count
+FROM summary
 WHERE item_type = 'prime_eligible'
 
 UNION ALL
 
 SELECT item_type,
-  FLOOR((500000 - (SELECT FLOOR(500000/sqf) * sqf FROM summary WHERE item_type = 'prime_eligible'))
-        / sqf) * item_count AS item_count
-FROM cte 
+       FLOOR((500000 - (SELECT FLOOR(500000/sqf)*sqf FROM summary 
+       WHERE item_type = 'prime_eligible')) / sqf) * item_count AS item_count
+FROM summary
 WHERE item_type = 'not_prime'
