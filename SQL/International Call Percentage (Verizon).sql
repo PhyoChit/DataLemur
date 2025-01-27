84b6c184e261a8e45c32da05d81f47b85f4dc47a
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2025-01-28 00:13:28 +0100

    Update International Call Percentage (Verizon).sql

--- SQL/International Call Percentage (Verizon).sql
+++ SQL/International Call Percentage (Verizon).sql
@@ -1,1 +1,17 @@
+WITH international_call AS (
+  SELECT 
+      caller.caller_id,
+      caller.country_id,
+      receiver.caller_id,
+      receiver.country_id
+  FROM phone_calls AS call 
+  JOIN phone_info AS caller 
+  ON call.caller_id = caller.caller_id 
+  JOIN phone_info AS receiver 
+  ON call.receiver_id = receiver.caller_id
+  WHERE caller.country_id <> receiver.country_id
+)
 
+SELECT 
+  ROUND(COUNT(*) * 100.0/(SELECT COUNT(*) FROM phone_calls), 1)
+FROM international_call
