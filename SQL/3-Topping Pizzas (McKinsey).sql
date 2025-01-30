f4dcc97552a535a389efe57bb5793ed40c435d87
Ppsc01 <52832393+PhyoChit@users.noreply.github.com>
2025-01-30 17:43:49 +0100

    Update 3-Topping Pizzas (McKinsey).sql

--- SQL/3-Topping Pizzas (McKinsey).sql
+++ SQL/3-Topping Pizzas (McKinsey).sql
@@ -8,3 +8,14 @@ ON p1.topping_name < p2.topping_name
 JOIN pizza_toppings As p3
 ON p2.topping_name < p3.topping_name
 ORDER BY total_cost DESC, pizza;
+
+-- Option 2: Using CROSS JOIN
+SELECT 
+  CONCAT(p1.topping_name , ',' , p2.topping_name , ',' , p3.topping_name) AS pizza,
+  p1.ingredient_cost + p2.ingredient_cost + p3.ingredient_cost AS total_cost
+FROM pizza_toppings AS p1
+CROSS JOIN pizza_toppings AS p2
+CROSS JOIN pizza_toppings As p3
+WHERE p1.topping_name < p2.topping_name
+AND p2.topping_name < p3.topping_name
+ORDER BY total_cost DESC, pizza;
